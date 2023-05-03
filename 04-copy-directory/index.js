ad59a5919ac785dec43d8e006484b65611ff1a99
Sasha <pilevichsasha@gmail.com>
2023-05-03 17:28:18 +0300

    feat:add function for copy files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require("node:fs/promises");
+const path = require("path");
+const { copyFile } = require("node:fs/promises");
 
+const pathToFiles = path.join(__dirname, "files");
+const pathToCopyFiles = path.join(__dirname, "files-copy");
+const option = { withFileTypes: true };
+
+async function getFileFromFolder(pathToFiles, option) {
+  try {
+        await fs.rm(pathToCopyFiles, { recursive: true, force:true });
+        await fs.mkdir(pathToCopyFiles);
+        const filesForCopy = fs.readdir(pathToFiles, option);
+        (await filesForCopy).forEach((item) => {
+          copyFile(
+            path.join(pathToFiles, item.name),
+            path.join(pathToCopyFiles, item.name)
+          );
+        });
+      
+  }catch (error) {
+    console.log(error.message);
+  }
+}
+getFileFromFolder(pathToFiles, option);
